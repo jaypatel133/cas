3fac0c5c82197631b4895d99ad33c4a441b329dc
jaypatel133 <67822461+jaypatel133@users.noreply.github.com>
2023-05-07 00:24:41 +0530

    first commit

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,35 @@
+import React, {useRef, useEffect} from 'react';
+import { StyleSheet, Text, View, Image, Animated     } from 'react-native';
+
+function WelcomeScreen({navigation}) {
+
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+    
+    useEffect(() => {
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 3000,
+          useNativeDriver: true,
+        }).start();
+      }, [fadeAnim]);
+
+    useEffect(()=> {
+      setTimeout(function(){
+        navigation.navigate('Login')
+   
+      }, 4000);
+    },[]) 
+
+    return (
+
+        <Animated.View style={{ justifyContent: 'center', alignItems: 'center', height: '100%', opacity: fadeAnim}}>
+
+           <Image source={require('../assets/logo.png')}/>  
+        
+        </Animated.View>
+            
+        
+    );
+}
+
+export default WelcomeScreen;
